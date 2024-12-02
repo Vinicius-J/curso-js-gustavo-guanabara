f363f81b51cc2fa2ba07eaef791cccaef0d53d0f
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2024-12-02 20:49:24 -0300

    Aula07

--- aula04_05/js/script.js
+++
@@ -1,3 +0,0 @@
-window.alert('Meu primeiro programa');
-window.confirm('Você está gostando do curso de JS?');
-window.prompt('Qual seu nome?');
--- aula06/js/scritp.js
+++
@@ -1,3 +0,0 @@
-window.alert('Meu primeiro programa!');
-window.confirm('Estrá gostando do curso de JS?');
-window.prompt('Qual é o seu nome?');
